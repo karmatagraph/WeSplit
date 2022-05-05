2817d1168d17ea5b2f0a2ba2a73557524e82976b
karma <karmatashi954@gmail.com>
2022-05-05 11:06:11 +0545

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by karma on 5/5/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
